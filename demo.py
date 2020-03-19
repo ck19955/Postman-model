d19f0f2844ec98508578bd21bd7bbab19712025d
Stephen <ck19955@bristol.ac.uk>
2020-03-19 16:04:22 +0000

    Code can now vary number pf houses

--- demo.py
+++ demo.py
@@ -3,6 +3,10 @@ from postman.objects import BusPassenger, Postman, House
 from postman.linear import LinearBusRouteModel
 from postman.animate import animate_model
 
+NumHouses = 10
+MailPerHouse = 2
+
+
 House1_mail = [ 
     BusPassenger('', 'Cart', 'House 1'),
     BusPassenger('', 'Cart', 'House 1'),
@@ -29,18 +33,16 @@ House4_mail = [
 
 Mail = House1_mail + House2_mail + House3_mail + House4_mail
 
-houses = [
-    House('Cart', (0, 0), Mail),
-    House('House 1', (25, 0), []),
-    House('House 2', (50, 0), []),
-    House('House 3', (75, 0), []),
-    House('House 4', (100, 0), []),
-]
+Houses = []
+HouseSpacing = 100/NumHouses
+for i in range(1,NumHouses+1):
+    Houses.append(House("House " + str(i), (int((i)*HouseSpacing),0), []))
+
 
 postman = [
-    Postman('Postman', (0,0), 1, [], 1.0),
+    Postman('Postman', (0,0), 1, Mail, 1.0),
 ]
 
-model = LinearBusRouteModel(0, 100, houses, postman)
+model = LinearBusRouteModel(0, 100, Houses, postman)
 
 animate_model(model)
